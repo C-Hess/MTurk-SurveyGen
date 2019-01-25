216433e19398af23410aff7e53f852798148ec11
Cameron Hessler <cameron@Camerons-MacBook-Air.local>
2019-01-24 23:32:36 -0600

    Fixed copy+paste issue with mac builds

--- main.js
+++ main.js
@@ -1,36 +1,57 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, Menu, BrowserWindow } = require("electron");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    minWidth: 650,
-    minHeight: 500
-  });
-  mainWindow.setTitle("Mechanical Turk Survey Generator");
-  // and load the index.html of the app.
-  mainWindow.loadFile("build/index.html");
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		minWidth: 650,
+		minHeight: 500
+	});
+	mainWindow.setTitle("Mechanical Turk Survey Generator");
+	// and load the index.html of the app.
+	mainWindow.loadFile("build/index.html");
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	// Open the DevTools.
+	// mainWindow.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  mainWindow.on("closed", function() {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+	// Emitted when the window is closed.
+	mainWindow.on("closed", function() {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null;
+	});
 
-  //BrowserWindow.addDevToolsExtension(
-  //  "/home/cameron/.config/google-chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/3.4.0_0"
-  //);
+	// Check if we are on a MAC
+	if (process.platform === 'darwin') {
+		// Create our menu entries so that we can use MAC shortcuts
+		Menu.setApplicationMenu(Menu.buildFromTemplate([
+			{
+				label: 'Edit',
+				submenu: [
+					{ role: 'undo' },
+					{ role: 'redo' },
+					{ type: 'separator' },
+					{ role: 'cut' },
+					{ role: 'copy' },
+					{ role: 'paste' },
+					{ role: 'pasteandmatchstyle' },
+					{ role: 'delete' },
+					{ role: 'selectall' }
+				]
+			}
+		]));
+	}
+
+	//BrowserWindow.addDevToolsExtension(
+	//  "/home/cameron/.config/google-chrome/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/3.4.0_0"
+	//);
 }
 
 // This method will be called when Electron has finished
@@ -40,20 +61,21 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
 
 app.on("activate", function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
